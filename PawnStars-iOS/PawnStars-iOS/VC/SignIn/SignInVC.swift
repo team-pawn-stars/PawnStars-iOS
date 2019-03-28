3b565ca08c5cfcd38a0d9265cff12a1434386c0c
Jeongdasilver <alal8272@gmail.com>
2019-03-28 10:42:38 +0900

    [ADD] ADD Concept UI (SignIn, SignUp)

---
+++ PawnStars-iOS/PawnStars-iOS/VC/SignIn/SignInVC.swift
@@ -0,0 +1,21 @@
+//
+//  SignInVC.swift
+//  PawnStars-iOS
+//
+//  Created by daeun on 27/03/2019.
+//  Copyright © 2019 PawnStars. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+}
---
+++ PawnStars-iOS/PawnStars-iOS/VC/SignIn/SignUpVC.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpVC.swift
+//  PawnStars-iOS
+//
+//  Created by daeun on 27/03/2019.
+//  Copyright © 2019 PawnStars. All rights reserved.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+    
+    }
+}
+
+
+class SignUpContentVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

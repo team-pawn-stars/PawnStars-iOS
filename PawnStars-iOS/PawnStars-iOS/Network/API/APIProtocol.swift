0bedf93671948839a9361cb770f376d842581165
Jeongdasilver <alal8272@gmail.com>
2019-04-17 10:27:03 +0900

    Revert "Revert "Merge branch 'SignIn' of https://github.com/team-pawn-stars/PawnStars-iOS into SignIn""
    This reverts commit 695a43b277d9334b12272ba3a49fe52e399fce83.

---
+++ PawnStars-iOS/PawnStars-iOS/Network/API/APIProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  APIProtocol.swift
+//  PawnStars-iOS
+//
+//  Created by 조우진 on 15/04/2019.
+//  Copyright © 2019 PawnStars. All rights reserved.
+//
+
+import Foundation
+
+protocol API {
+    func getPath() -> String
+}
---
+++ PawnStars-iOS/PawnStars-iOS/Network/Connector.swift
@@ -0,0 +1,54 @@
+//
+//  Connector.swift
+//  PawnStars-iOS
+//
+//  Created by 조우진 on 17/04/2019.
+//  Copyright © 2019 PawnStars. All rights reserved.
+//
+
+import Alamofire
+import RxAlamofire
+import RxSwift
+import RxCocoa
+
+protocol ConnectorType {
+    func get(path: String, params: Parameters, header : Header) -> Observable<(HTTPURLResponse,Data)>
+    func post(path: String, params: Parameters, header : Header) -> Observable<(HTTPURLResponse,Data)>
+}
+
+class Connector : ConnectorType {
+    let baseUrl = "http://whale.istruly.sexy:3214/"
+
+    func get(path: String, params: Parameters, header: Header) -> Observable<(HTTPURLResponse, Data)> {
+        return requestData(.get,
+                           baseUrl + path,
+                           parameters: params,
+                           encoding: URLEncoding.queryString,
+                           headers: header.getHeader())
+    }
+    
+    func post(path: String, params: Parameters, header: Header) -> Observable<(HTTPURLResponse, Data)> {
+        return requestData(.post,
+                           baseUrl + path,
+                           parameters: params,
+                           encoding: JSONEncoding.default,
+                           headers: header.getHeader())
+    }
+    
+}
+
+enum Header {
+    case Authorization,Empty
+    func getHeader() -> [String : String]? {
+        switch self {
+        case .Authorization:
+            return ["Authorization" : UserDefaults.standard.value(forKey: "Token") as? String ?? "",
+                    "Content-Type" : "application/json"]
+        case .Empty :
+            return ["Content-Type" : "application/json"]
+        }
+    }
+}
+
+
+

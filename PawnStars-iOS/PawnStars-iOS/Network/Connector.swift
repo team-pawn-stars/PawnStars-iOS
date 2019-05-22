a5f7a387d9a39971cd000136e59ff775ef2d81c4
Jeongdasilver <alal8272@gmail.com>
2019-05-22 22:39:30 +0900

    [UPDATE] Connector
    add patch, delete function
    update token

--- PawnStars-iOS/PawnStars-iOS/Network/Connector.swift
+++ PawnStars-iOS/PawnStars-iOS/Network/Connector.swift
@@ -35,6 +35,20 @@ class Connector : ConnectorType {
                            headers: header.getHeader())
     }
     
+    func patch(path: String, params: Parameters?, header: Header) -> Observable<(HTTPURLResponse, Data)> {
+        return requestData(.patch,
+                           baseUrl + path,
+                           parameters: params,
+                           encoding: JSONEncoding.default,
+                           headers: header.getHeader())
+    }
+    func delete(path: String, params: Parameters?, header: Header) -> Observable<(HTTPURLResponse, Data)> {
+        return requestData(.delete,
+                           baseUrl + path,
+                           parameters: params,
+                           encoding: JSONEncoding.default,
+                           headers: header.getHeader())
+    }
 }
 
 enum Header {
@@ -42,7 +56,8 @@ enum Header {
     func getHeader() -> [String : String]? {
         switch self {
         case .Authorization:
-            return ["Authorization" : UserDefaults.standard.value(forKey: "Token") as? String ?? "",
+            let token = UserDefaults.standard.value(forKey: "Token") as? String ?? ""
+            return ["Authorization" : "Bearer \(token)",
                     "Content-Type" : "application/json"]
         case .Empty :
             return ["Content-Type" : "application/json"]

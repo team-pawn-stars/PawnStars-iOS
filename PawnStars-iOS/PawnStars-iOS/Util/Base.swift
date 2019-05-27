f6654432b3ecf7b1fdd25ccc352ccda5d38aa67a
Woopa01 <whdnwls49@naver.com>
2019-05-27 11:06:49 +0900

    [UPDATE] add viewWillAppear rx controlevent

--- PawnStars-iOS/PawnStars-iOS/Util/Base.swift
+++ PawnStars-iOS/PawnStars-iOS/Util/Base.swift
@@ -8,8 +8,18 @@
 
 import Foundation
 import UIKit
+import RxSwift
+import RxCocoa
 
 extension UIViewController{
     // static method or property
+    
+}
+
+extension Reactive where Base: UIViewController {
+    var viewWillAppear: ControlEvent<Void> {
+        let source = self.methodInvoked(#selector(Base.viewWillAppear(_:))).map { _ in }
+        return ControlEvent(events: source)
+    }
 }
 

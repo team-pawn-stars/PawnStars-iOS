69a4736cff201ea91e55c13772fe835aa0d9af68
Woopa01 <whdnwls49@naver.com>
2019-05-27 11:10:40 +0900

    [ADD] SelectRegionVC

---
+++ PawnStars-iOS/PawnStars-iOS/VC/PawnList/SelectRegionVC.swift
@@ -0,0 +1,39 @@
+//
+//  SelectRegionVC.swift
+//  PawnStars-iOS
+//
+//  Created by 조우진 on 14/05/2019.
+//  Copyright © 2019 PawnStars. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import UIKit
+
+class SelectRegionVC: UIViewController {
+    @IBOutlet weak var regionPickerView: UIPickerView!
+    
+    var viewModel: PawnListViewModel!
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        bindViewModel()
+    }
+}
+
+extension SelectRegionVC {
+    func bindViewModel(){
+        viewModel = PawnListViewModel()
+        
+        Observable
+            .just(["서울","광주","대전","대구","부산","울산","인천","강원","경기","경남","전북","전남","제주","충북","충남"])
+            .bind(to: regionPickerView.rx.itemTitles) { _, element in
+                return "\(element)"
+            }.disposed(by: disposeBag)
+        
+        let input = PawnListViewModel.ThirdInput(region: regionPickerView.rx.modelSelected(String.self).asDriver())
+        
+        viewModel.thirdTransform(input: input)
+    }
+}

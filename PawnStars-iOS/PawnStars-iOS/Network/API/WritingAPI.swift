fb163b1853779b4ecef0fca4e3bd2ad6d55cabc6
Jeongdasilver <alal8272@gmail.com>
2019-05-31 09:54:50 +0900

    [ADD] WritingAPI

---
+++ PawnStars-iOS/PawnStars-iOS/Network/API/WritingAPI.swift
@@ -0,0 +1,24 @@
+//
+//  WritingAPI.swift
+//  PawnStars-iOS
+//
+//  Created by daeun on 29/05/2019.
+//  Copyright © 2019 PawnStars. All rights reserved.
+//
+
+import Foundation
+
+public enum WritingAPI: API{
+    
+    case writePawn
+    case writePawnImage
+    case writePawnHistory
+    
+    func getPath() -> String {
+        switch self {
+        case .writePawn: return "pawn/"
+        case .writePawnImage: return "pawn/image/"
+        case .writePawnHistory: return "pawn/history/"
+        }
+    }
+}
